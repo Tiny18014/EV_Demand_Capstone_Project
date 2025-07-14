57f6e9cc29533251afb2b4d9dd021fff1710d0f8
theboredflamingo <stuthi.shrisha@gmail.com>
2025-07-14 11:32:48 +0530

    added feature and target set update

--- src/model/train.py
+++ src/model/train.py
@@ -14,7 +14,8 @@ end_str = end_date.strftime('%Y-%m-%d')
 
 # Step 3: Preprocess data using dynamic date strings
 x, y = preprocess(start_str, end_str)
-
+x.to_csv("data/feat.csv", index=False)
+y.to_csv("data/targ.csv", index=False)
 # Step 4: Load existing model
 with open("src/model/rm1.pkl", "rb") as f:
     model = pickle.load(f)
